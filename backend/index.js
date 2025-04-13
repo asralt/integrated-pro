e855901ddaf0cfe73bd9e5e501ca7e73a11f0023
aska <enkhbadralasral00@gmail.com>
2025-04-13 13:48:47 +0100

    added const path =require("path")

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); 
+const path = require("path");
+
 const nodemailer = require("nodemailer");
 
 const app = express();
