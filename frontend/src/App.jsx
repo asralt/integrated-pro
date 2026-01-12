9d7901a5767b9ca396b977cdd0994aae0949511b
aska <enkhbadralasral00@gmail.com>
2026-01-12 18:03:27 +0000

    swapped APP for simple to check for error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,40 @@
+// import React from 'react'
+// import Contact from './pages/Contact'
+// import Hero from './pages/Hero'
+// import Projects from './pages/Projects'
+// import Navbar from './components/Navbar'
+// import Social from './components/Social'
+
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Router>
+//       <Navbar/>
+//         <div className=''> 
+//             <Routes>
+//             <Route path="/" element={<Hero/>}/>
+//             <Route path="/projects" element={<Projects/>}/>
+//             <Route path="/contact" element={<Contact/>}/>
+//             </Routes>
+//         </div>
+//      </Router>
+//       <div className="flex  items-center w-full mt-0.5 justify-start pl-[45%]">
+//         <Social />
+//       </div>
+
+//     </div>
+//   )
+// }
+
+// export default App
 import React from 'react'
-import Contact from './pages/Contact'
-import Hero from './pages/Hero'
-import Projects from './pages/Projects'
-import Navbar from './components/Navbar'
-import Social from './components/Social'
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <Router>
-      <Navbar/>
-        <div className=''> 
-            <Routes>
-            <Route path="/" element={<Hero/>}/>
-            <Route path="/projects" element={<Projects/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-            </Routes>
-        </div>
-     </Router>
-      <div className="flex  items-center w-full mt-0.5 justify-start pl-[45%]">
-        <Social />
-      </div>
-
-    </div>
+    <div>App</div>
   )
-}
-
-export default App
+} 
