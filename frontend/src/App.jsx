aee05e19d6427d2aefd5d9fe9c0504084227c44e
aska <enkhbadralasral00@gmail.com>
2026-01-12 18:08:23 +0000

    put back the removed code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,32 @@
-// import React from 'react'
-// import Contact from './pages/Contact'
-// import Hero from './pages/Hero'
-// import Projects from './pages/Projects'
-// import Navbar from './components/Navbar'
-// import Social from './components/Social'
-
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
-// const App = () => {
-//   return (
-//     <div>
-//       <Router>
-//       <Navbar/>
-//         <div className=''> 
-//             <Routes>
-//             <Route path="/" element={<Hero/>}/>
-//             <Route path="/projects" element={<Projects/>}/>
-//             <Route path="/contact" element={<Contact/>}/>
-//             </Routes>
-//         </div>
-//      </Router>
-//       <div className="flex  items-center w-full mt-0.5 justify-start pl-[45%]">
-//         <Social />
-//       </div>
-
-//     </div>
-//   )
-// }
-
-// export default App
 import React from 'react'
+import Contact from './pages/Contact'
+import Hero from './pages/Hero'
+import Projects from './pages/Projects'
+import Navbar from './components/Navbar'
+import Social from './components/Social'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
-export default function App() {
-  return <h1>Hello World</h1>
+const App = () => {
+  return (
+    <div>
+      <Router>
+      <Navbar/>
+        <div className=''> 
+            <Routes>
+            <Route path="/" element={<Hero/>}/>
+            <Route path="/projects" element={<Projects/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            </Routes>
+        </div>
+     </Router>
+      <div className="flex  items-center w-full mt-0.5 justify-start pl-[45%]">
+        <Social />
+      </div>
+
+    </div>
+  )
 }
+
+export default App
